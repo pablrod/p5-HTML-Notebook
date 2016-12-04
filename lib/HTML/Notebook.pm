32efd39611f4939d9ca62db9be698a120674b5a8
Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
2016-12-04 23:56:43 +0100

    Add support for customizing some notebook parts

--- lib/HTML/Notebook.pm
+++ lib/HTML/Notebook.pm
@@ -4,6 +4,7 @@ use utf8;
 
 use Moose;
 use Text::Template;
+use HTML::Notebook::Style;
 use namespace::autoclean;
 
 # VERSION
@@ -26,16 +27,14 @@ Compose HTML documents using notebook style. Every notebook is composed of HTML:
 
 =cut
 
-has 'cells' => (
-	traits => ['Array'],
-	is => 'rw',
-	isa => 'ArrayRef[HTML::Notebook::Cell]',
-	default => sub { [] },
-	handles => {
-		add_cell => 'push',
-		get_cell => 'get',
-		delete_cell => 'delete',
-	}
+has 'cells' => ( traits  => ['Array'],
+                 is      => 'rw',
+                 isa     => 'ArrayRef[HTML::Notebook::Cell]',
+                 default => sub { [] },
+                 handles => { add_cell    => 'push',
+                              get_cell    => 'get',
+                              delete_cell => 'delete',
+                 }
 );
 
 =head2 render
@@ -45,39 +44,46 @@ Render object to HTML
 =cut
 
 sub render {
-	my $self = shift();
-    my $html_begin = <<'HTML_BEGIN';
+    my $self   = shift();
+    my %params = @_;
+    my $style  = $params{'style'} // HTML::Notebook::Style->new();
+    my $html   = <<'HTML';
+<!DOCTYPE html>
 <html>
 <head>
-<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/readable/bootstrap.min.css" rel="stylesheet" integrity="sha384-Li5uVfY2bSkD3WQyiHX8tJd0aMF91rMrQP5aAewFkHkVSTT2TmD2PehZeMmm7aiL" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<style>
-body    {padding-top: 25px;}
-.cell    {padding-left: 5%; padding-right: 5%; padding-top: 25px; padding-botton: 25px}
-</style>
+{$head}
 </head>
 <body>
-HTML_BEGIN
+<div id="notebook-header">
+{$header}
+</div>
+</div>
+<div id="notebook-body">
+{$body}
+</div>
+</body>
+</html>
+HTML
 
-	my $cell_template = <<'CELL';
+    my $cell_template = <<'CELL';
 <div class="cell" id="{$cell_id}">
 {$content}
 </div>
 CELL
 
-	my $body = "";
-	my $renderer = Text::Template->new(TYPE => 'STRING', SOURCE => $cell_template);
-	my $numeric_id = 0;
-	for my $cell (@{$self->cells}) {
-		$body .= $renderer->fill_in(HASH => {cell_id => 'cell_' . $numeric_id++,
-											 content => $cell->content});
-	}
+    my $body          = "";
+    my $renderer      = Text::Template->new( TYPE => 'STRING', SOURCE => $html );
+    my $cell_renderer = Text::Template->new( TYPE => 'STRING', SOURCE => $cell_template );
+    my $numeric_id    = 0;
+    for my $cell ( @{ $self->cells } ) {
+        $body .= $cell_renderer->fill_in(
+                                          HASH => { cell_id => 'cell_' . $numeric_id++,
+                                                    content => $cell->content
+                                          }
+        );
+    }
 
-    my $html_end = <<'HTML_END';
-</body>
-</html>
-HTML_END
-    return $html_begin . $body . $html_end;
+    return $renderer->fill_in( HASH => { body => $body, head => $style->head, header => $style->header } );
 }
 
 __PACKAGE__->meta->make_immutable();
